1ccd14bff87cc713eef5f1a730592c58396d9335
FOODOHOLIC <adarshkohli183@gmail.com>
2025-06-11 21:47:17 +0530

    new

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
 
-
+app.use(cookieParser());
 
 // app.get("/",(req,res)=>{
 //     bcrypt.genSalt(10,(err,salt)=>{
@@ -14,11 +16,25 @@ const bcrypt = require("bcrypt");
 // });
 
 //checking the pasword matches with the hash or not 
-app.get("/", (req, res) => {
-    bcrypt.compare("password", "$2b$10$GrOR1CM633ceZeCh2iUOCOUOkr/fMcgYA6P0NXsU52DxKJBo6iTDK", (err, result) => {
-        console.log(result);
-    });
+// app.get("/", (req, res) => {
+//     bcrypt.compare("password", "$2b$10$GrOR1CM633ceZeCh2iUOCOUOkr/fMcgYA6P0NXsU52DxKJBo6iTDK", (err, result) => {
+//         console.log(result);
+//     });
 
+// });
+
+//JWT 
+
+app.get("/",(req,res)=>{
+    let token = jwt.sign({email: "adarsh@gmail.com"},"secret");
+    res.cookie("token",token);
+    res.send("done");
+});
+
+app.get("/read",(req,res)=>{
+    //extractiong the data
+    let data = jwt.verify(req.cookies.token,"secret");
+    console.log(data);
 })
 
 
